825d63862fb0002d002313dbeb56925896a4370c
Andrew Tsolkas <atsolkas97@gmail.com>
2024-04-05 15:59:26 +0300

    ...

---
+++ front_end/src/pages/Page2.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function Page2() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h2>2</h2>
+  )
+}
+
+export default Page2;
