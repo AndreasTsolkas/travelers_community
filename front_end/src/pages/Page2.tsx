2fc80fb73f51ffa85737b887d7cd1b7fc89c4f7e
Andrew Tsolkas <atsolkas97@gmail.com>
2024-04-05 16:30:04 +0300

    Προσθήκη browser router

--- front_end/src/pages/Page2.tsx
+++ front_end/src/pages/Page2.tsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 function Page2() {
-  const [count, setCount] = useState(0)
 
   return (
     <h2>2</h2>
