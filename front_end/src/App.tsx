838bf8144b044735590ef8855a2424eeb55d94ca
Andrew Tsolkas <atsolkas97@gmail.com>
2024-04-08 22:05:57 +0300

    ...

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import 'src/App.css';
 import 'src/basic.css';
 import MyReads from 'src/pages/MyReads';
 import Suggestions from 'src/pages/Suggestions';
 import Navbar from 'src/Navbar';
 import Home from 'src/pages/Home';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
